0ab2d4291fa6257822514552eecd5cbacb78eba1
Mr.Knight <109825410+akon00@users.noreply.github.com>
2023-07-31 22:06:57 +0600

    Create config.js

---
+++ website/config/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+ debug: true, // Boolean
+ whitelist_enabled: false,
+ allowed_ips: ["::ffff:59.149.31.198"], // Array with IP's
+ bot: {
+  type: ["Ticket", "Chat", "Clan"],
+  statustype: ["Online", "Dnd", "Idle"],
+ },
+ teams: [
+  {
+   name: "ProGamerSoujanya",
+   avatar: "masterious.png",
+   description: "Founder of Coderz Hangout",
+   whatheis: "Founder",
+  },
+ ],
+};

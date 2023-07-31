9c2b1fadb0b685f36867639e5dd46fe21a99bc2c
Mr.Knight <109825410+akon00@users.noreply.github.com>
2023-07-31 22:14:34 +0600

    Add files via upload

---
+++ website/utilities/getdcname.js
@@ -0,0 +1,9 @@
+const BASE_URL = "https://api.discordz.xyz/discord/v1/user?id=";
+const fetch = require("node-fetch");
+async function getdcname(botid) {
+ await fetch(BASE_URL + botid)
+  .then((res) => res.json())
+  .then((body) => console.log(body))
+  .catch((e) => console.log(e));
+}
+module.exports = getdcname;
